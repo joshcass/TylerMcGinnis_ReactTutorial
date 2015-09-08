33d0f2350c6347a66100da0dceabc9d043611964
Josh Cass <joshuacass@gmail.com>
2015-09-07 21:03:48 -0600

    Added Browserify for dev and production builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,59 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var react = require('gulp-react');
 var htmlreplace = require('gulp-html-replace');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var streamify = require('gulp-streamify');
 
 var path = {
     HTML: 'src/index.html',
-    ALL: ['src/js/*.js', 'src/js/**/*.js', 'src/index.html'],
-    JS: ['src/js/*.js', 'src/js/**/*.js'],
     MINIFIED_OUT: 'build.min.js',
-    DEST_SRC: 'dist/src',
+    OUT: 'build.js',
+    DEST: 'dist',
     DEST_BUILD: 'dist/build',
-    DEST: 'dist'
+    DEST_SRC: 'dist/src',
+    ENTRY_POINT: './src/js/App.js'
 };
 
-gulp.task('transform', function(){
-    gulp.src(path.JS)
-        .pipe(react())
+gulp.task('replaceHTMLsrc', function(){
+    gulp.src(path.HTML)
+        .pipe(htmlreplace({
+            'js': 'src/' + path.OUT
+        }))
+        .pipe(gulp.dest(path.DEST));
+});
+
+gulp.task('watch', ['replaceHTMLsrc'], function() {
+    gulp.watch(path.HTML, ['replaceHTMLsrc']);
+
+    var watcher  = watchify(browserify({
+        entries: [path.ENTRY_POINT],
+        transform: [reactify],
+        debug: true,
+        cache: {}, packageCache: {}, fullPaths: true
+    }));
+
+    return watcher.on('update', function () {
+        watcher.bundle()
+            .pipe(source(path.OUT))
+            .pipe(gulp.dest(path.DEST_SRC));
+        console.log('Updated');
+    })
+        .bundle()
+        .pipe(source(path.OUT))
         .pipe(gulp.dest(path.DEST_SRC));
 });
 
-gulp.task('copy', function(){
-    gulp.src(path.HTML)
-        .pipe(gulp.dest(path.DEST));
-});gulp.task('watch', function(){
-    gulp.watch(path.ALL, ['transform', 'copy']);
-});
-
-gulp.task('watch', function(){
-    gulp.watch(path.ALL, ['transform', 'copy']);
-});
-
 gulp.task('build', function(){
-    gulp.src(path.JS)
-        .pipe(react())
-        .pipe(concat(path.MINIFIED_OUT))
-        .pipe(uglify({file: path.MINIFIED_OUT }))
+    browserify({
+        entries: [path.ENTRY_POINT],
+        transform: [reactify]
+    })
+        .bundle()
+        .pipe(source(path.MINIFIED_OUT))
+        .pipe(streamify(uglify({file: path.MINIFIED_OUT })))
         .pipe(gulp.dest(path.DEST_BUILD));
 });
 
